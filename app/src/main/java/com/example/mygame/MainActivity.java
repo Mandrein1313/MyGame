96cf978705cd64ec2c72b8ce91f6293de7750cfd
root <root@localhost>
2026-06-02 08:28:04 +0700

    Cloud Build Request - 2026-06-02 08:28:04

---
+++ app/src/main/java/com/example/mygame/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mygame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.mygame.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1322);
+    }
+}
